b4f3f5e3f789f64a79fc5f9c164a638829c55876
Jaume Antoni Ferrando Vidal <ferrandinyo@uoc.edu>
2024-04-21 22:27:32 +0200

    alguns retocs

--- src/js/detalls.js
+++ src/js/detalls.js
@@ -4,17 +4,10 @@ const paginaActual = location.href.split("/").slice(-1);
 
 const index = paginesDetall.indexOf(paginaActual[0]);
 
-console.log(index);
-
 const seguent = paginesDetall.at((index + 1) % paginesDetall.length);
 
-console.log(seguent);
-
 const anterior = paginesDetall.at(index - 1);
-
-console.log(anterior);
 
 document.querySelector(".anterior").href = anterior;
 
 document.querySelector(".seguent").href = seguent;
-
